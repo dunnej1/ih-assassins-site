cc82c3e8d59bfea4460d82ca9a3af91516938106
dunnej1 <dunnej1@student.unimelb.edu.au>
2018-08-15 15:28:58 +1000

    second commit

---
+++ assassins/src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h4>International House | CIA Week</h4>
+                <p> blah blah blah blah blah blah blah blah blah</p>
+
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ assassins/src/Leaderboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Leaderboard extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Leaderboard</h2>
+                <p>INSERT LEADERBOARD HERE</p>
+            </div>
+        );
+    }
+}
+
+export default Leaderboard;
